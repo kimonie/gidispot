f34809e9780c71a89fb40aef443223baf7d26d62
unknown <seunkikiowo@gmail.com>
2023-02-20 23:09:33 +0100

    added the enviroment variable and initiated the back ground

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
+
+mongoose.set('strictQuery', true);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
@@ -14,12 +17,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL =  'mongodb+srv://kimonie:uzLrlRVr8Uiya9On@cluster0.pvah8dk.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL =  'mongodb+srv://kimonie:uzLrlRVr8Uiya9On@cluster0.pvah8dk.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-    mongoose.set('strictQuery', false);
+    
 // mongoose.set('useFindAndModify', false);
