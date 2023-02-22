7bb96b16d92cc39d3257b9c87b04e9ab0d47b9b7
unknown <seunkikiowo@gmail.com>
2023-02-22 23:57:13 +0100

    adding loging features to the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts';
